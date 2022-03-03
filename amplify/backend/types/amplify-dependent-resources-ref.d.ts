1379e3b9439b42c2d38eef161e0d19da66b4205c
Hiter Harris <hiterharris@comcast.net>
2022-03-03 18:14:17 -0500

    add auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "amplifycustomschemae410eeed": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react'
 import './App.css'
 import useContent from './helpers/useContent'
 import UpdateModal from './components/UpdateModal'
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-const App = () => {
+const App = ({ signOut, user }) => {
   const { data, fetchData } = useContent()
+  console.log(user)
   
   return (
     <div className="App">
+      <h1>Hello {user.attributes.email}</h1>
       <h1>{data?.headerTitle}</h1>
       <h3>{data?.headerSubtitle}</h3>
       <UpdateModal content={data} fetchData={fetchData} />
+      <>
+      <button className="signout-button" onClick={signOut}>Sign out</button>
+      </>
+
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+  
